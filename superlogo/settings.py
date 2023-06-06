493609e110678504c463b208c3cef0cfd5a4969e
Tufail Akram <72789060+akram1089@users.noreply.github.com>
2023-06-06 15:42:06 +0530

    reset password added

--- superlogo/settings.py
+++ superlogo/settings.py
@@ -158,3 +158,13 @@ MESSAGE_TAGS = {
         messages.WARNING: 'warning',
         messages.ERROR: 'danger',
  }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+# EMAIL_USE_SSL = True
+EMAIL_HOST_USER = 'tufailakram8190@gmail.com'
+EMAIL_HOST_PASSWORD = 'sxwwgptgifiljqsj'
+
