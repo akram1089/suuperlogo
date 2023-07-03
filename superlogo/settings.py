ab37e8c9b327956ccf130182df48b1fbdc9b8e8f
Tufail Akram <72789060+akram1089@users.noreply.github.com>
2023-07-03 15:43:54 +0530

    vercel setup

--- superlogo/settings.py
+++ superlogo/settings.py
@@ -108,6 +108,7 @@ AUTHENTICATION_BACKENDS = [
 
     # `allauth` specific authentication methods, such as login by e-mail
     'allauth.account.auth_backends.AuthenticationBackend',
+    "django.contrib.auth.backends.ModelBackend",
 ]
 
 
